19df63803b92f12050d02848cac80964408e9477
shakir <shakkir2227@gmail.com>
2024-05-18 17:28:04 +0530

    DB atlas connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`server listening on port 3000`)
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log(`Database connected successfully`)
+    app.listen(3000, () => {
+        console.log(`server listening on port 3000`)
+    })
 })
+.catch((err) => {
+    console.log(`Database connnection error: ${err}`)
+})
+
