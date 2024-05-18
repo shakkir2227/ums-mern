886bd866b7f8d208ad5ab0c8b4d77600cbdab580
shakir <shakkir2227@gmail.com>
2024-05-18 17:38:34 +0530

    user model created

--- api/index.js
+++ api/index.js
@@ -7,13 +7,13 @@ dotenv.config()
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log(`Database connected successfully`)
-    app.listen(3000, () => {
-        console.log(`server listening on port 3000`)
+.then((connectionInstance) => {
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`); 
+    app.listen(process.env.PORT || 3000,  () => {
+        console.log(`server listening on port ${process.env.PORT}`)
     })
 })
 .catch((err) => {
-    console.log(`Database connnection error: ${err}`)
+    console.log(`Database connection error: ${err}`)
 })
 
