b7c56e68989454a78871c390e8ab4c066590e875
shakir <shakkir2227@gmail.com>
2024-05-19 18:08:42 +0530

    user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,35 @@
+import User from "../models/user.model.js"
+
 export const test = (req, res) => {
     res.send(`Home`)
+}
+
+export const updateUser = async (req, res, next) => {
+
+    // if user is not authenticated, throw error
+    if (!req.user) return next(errorHandler(401, 'You need to Login!!'))
+
+    const { user } = req;
+    const { profilePicture } = req.body
+
+    // if there is no profile picture, return
+    if (!profilePicture) {
+        return res.status(200).json({
+            message: `Please select a profile picture before updating. 
+        No picture was selected or sent to the server.`})
+    }
+
+    try {
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: user.id },
+            { $set: { profilePicture } },
+            { new: true }
+        );
+        const { password, ...userDetails } = updatedUser._doc;
+
+        return res.status(200).json(userDetails)
+
+    } catch (error) {
+        console.log(error)
+    }
 }
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config()
@@ -8,6 +9,7 @@ dotenv.config()
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO_URL)
     .then((connectionInstance) => {
